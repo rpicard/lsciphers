0d0b9196969bb50916dba08f42ddaec7ffd0cbde
Robert Picard <mail@robert.io>
2015-01-29 17:33:10 -0800

    SSL2 client hello achieved

--- lsciphers.go
+++ lsciphers.go
@@ -3,58 +3,36 @@ package main
 import "net"
 import "fmt"
 
-func make_ssl2_hello(cipher []uint8) []byte {
-
-    x := make([]uint8, 28)
-
-    // MSG-CLIENT-HELLO
-    x[0] = uint8(0x01)
-
-    // CLIENT-VERSION-MSB
-    x[1] = uint8(0x00)
-
-    // CLIENT-VERSION-LSB
-    x[2] = uint8(0x02)
-
-    // CIPHER-SPECS-LENGTH-MSB
-    x[3] = uint8(0x00)
-
-    // CIPHER-SPECS-LENGTH-LSB
-    x[4] = uint8(0x03)
-
-    // SESSION-ID-LENGTH-MSB
-    x[5] = uint8(0x00)
-
-    // SESSION-ID-LENGTH-LSB
-    x[6] = uint8(0x00)
-
-    // CHALLENGE-LENGTH-MSB
-    x[7] = uint8(0x00)
-
-    // CHALLENGE-LENGTH-LSB
-    x[8] = uint8(0x10)
-
-    // CIPHER-SPECS-DATA
-    x[9] = cipher[0]
-    x[10] = cipher[1]
-    x[11] = cipher[2]
-
-    // no SESSION-ID-DATA
-
-    // CHALLENGE-DATA is whatever garbage is at the end
-
-    return x
-}
-
 
 func main() {
+
+    SSL2_HELLO := []uint8{
+        0x80, 0x2e,                 // record length
+        0x01,                       // client hello
+        0x00, 0x02,                 // version
+        0x00, 0x15,                 // cipher specs length
+        0x00, 0x00,                 // session id length
+        0x00, 0x10,                 // challenge length
+        0x01, 0x00, 0x80,           // SSL_CK_RC4_128_WITH_MD5
+        0x02, 0x00, 0x80,           // SSL_CK_RC4_128_EXPORT40_WITH_MD5
+        0x03, 0x00, 0x80,           // SSL_CK_RC2_128_CBC_WITH_MD5
+        0x04, 0x00, 0x80,           // SSL_CK_RC2_128_CBC_EXPORT40_WITH_MD5
+        0x05, 0x00, 0x80,           // SSL_CK_IDEA_128_CBC_WITH_MD5
+        0x06, 0x00, 0x40,           // SSL_CK_DES_64_CBC_WITH_MD5
+        0x07, 0x00, 0xc0,           // SSL_CK_DES_192_EDE3_CBC_WITH_MD5
+        0xde, 0xad, 0xbe, 0xef,     // challenge data
+        0xde, 0xad, 0xbe, 0xef,
+        0xde, 0xad, 0xbe, 0xef,
+        0xde, 0xad, 0xbe, 0xef,
+    }
+
     // Read the host:port from ARGV
 
     // Try connecting with each protocol
 
-    conn, _ := net.Dial("tcp", "duckduckgo.com:443")
+    conn, _ := net.Dial("tcp", "secure.goywam.com:443")
 
-    conn.Write(make_ssl2_hello([]uint8{0x01, 0x00, 0x80}))
+    conn.Write(SSL2_HELLO)
 
     response := make([]uint8, 32)
     conn.Read(response)
