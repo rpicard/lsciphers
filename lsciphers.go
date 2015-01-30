3d89fba51da71b627d84911ad346e9d18ab01b0a
Robert Picard <mail@robert.io>
2015-01-29 16:28:02 -0800

    Move to simple functions

--- lsciphers.go
+++ lsciphers.go
@@ -1,12 +1,44 @@
-type SSL2_CLIENT_HELLO struct {
-    MSG-CLIENT-HELLO    uint8
-    CLIENT-VERSION      uint16
-    CIPHER-SPECS-LENGTH uint16
-    SESSION-ID-LENGTH   uint16
-    CHALLENGE-LENGTH    uint16
-    CIPHER-SPECS-DATA   []uint8
-    SESSSION-ID-DATA    []uint8
-    CHALLENGE-DATA      []uint8
+func make_ssl2_hello(cipher []uint8) []byte {
+
+    x := make([]uint8, 28)
+
+    // MSG-CLIENT-HELLO
+    x[0] = uint8(0x01)
+
+    // CLIENT-VERSION-MSB
+    x[1] = uint8(0x00)
+
+    // CLIENT-VERSION-LSB
+    x[2] = uint8(0x02)
+
+    // CIPHER-SPECS-LENGTH-MSB
+    x[3] = uint8(0x00)
+
+    // CIPHER-SPECS-LENGTH-LSB
+    x[4] = uint8(0x03)
+
+    // SESSION-ID-LENGTH-MSB
+    x[5] = uint8(0x00)
+
+    // SESSION-ID-LENGTH-LSB
+    x[6] = uint8(0x00)
+
+    // CHALLENGE-LENGTH-MSB
+    x[7] = uint8(0x00)
+
+    // CHALLENGE-LENGTH-LSB
+    x[8] = uint8(0x10)
+
+    // CIPHER-SPECS-DATA
+    x[9] = cipher[0]
+    x[10] = cipher[1]
+    x[11] = cipher[2]
+
+    // no SESSION-ID-DATA
+
+    // CHALLENGE-DATA is whatever garbage is at the end
+
+    return x
 }
 
 // Read the host:port from ARGV
